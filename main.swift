e98c5f0142363102eb1e46d80252a8625436fa5d
Francisco Garache <franciscogarach3@gmail.com>
2022-05-17 14:31:27 +0000

    animales

--- main.swift
+++ main.swift
@@ -11,7 +11,7 @@ shoppingList += ["Totopos", "Guacamole", "Pico de Gallo"]
 //cuantos itens hay en listas
 var num = shoppingList.count
 print(num)
-print("=====================")
+print("==========================================")
 
 //cambiar  itens
 shoppingList[0] = "arroz"
@@ -30,28 +30,28 @@ shoppingList[0] = "arroz"
 for (idx, item) in shoppingList.enumerated(){
     print("Item \(idx+1): \(item) ")
 }
-print("=====================")
+print("==========================================")
 
 //recorrer lista e imprimir
 for item in shoppingList{
     print(item)
 }
-print("=====================")
+print("==========================================")
  var primerElemento = shoppingList[0]
 
  var todosElemento = shoppingList[0...2]
 print(primerElemento)
 print(todosElemento)
-print("=====================")
+print("==========================================")
 shoppingList += ["ajos", "carne", "papas"]
 for item in shoppingList{
     print(item)
 }
-print("=====================")
+print("==========================================")
 for (idx, item) in shoppingList.enumerated(){
     print("Item \(idx+1): \(item) ")
 }
-print("=====================")
+print("==========================================")
 
 //Conjunto (Set) no existe orden
 var letters = Set<Character>()
@@ -73,3 +73,22 @@ if favouriteGames.isEmpty{
 if favouriteGames.contains("Metal Gear"){
     print("Me encanta ese juego")
 }
+
+let oddDigits: Set = [1,3,5,7,9]
+let evenDigits: Set = [0,2,4,6,8]
+let primeNumbers: Set = [2,3,5,7]
+print("==========================================")
+//A union B = elementos que son o bien de A, o bien de B o de los dos
+print(oddDigits.union(evenDigits).sorted())
+print("==========================================")
+
+//A intersección B = elementos que son a la vez de A y de B
+print(oddDigits.intersection(primeNumbers).sorted())
+// A - B = elementos que son de A pero no de B
+print(oddDigits.subtracting(primeNumbers).sorted())
+// A + B = (A-B) union (B-A)
+print(oddDigits.symmetricDifference(primeNumbers).sorted())
+print("==========================================")
+let houseAnimals:Set = ["🐶", "😹"]
+let farmAnimals:Set = ["🐮", "🐔", "🐑", "🐶", "😹"]
+let cityAnimals:Set = ["🐁", "🕊"]
